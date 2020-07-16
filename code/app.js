042e5c0a07725652a3065213ade669f4eda42e8e
Iuliia Filonenko <filonenkoyulia7@gmail.com>
2020-07-16 23:12:17 +0300

    Добавляет js

--- code/app.js
+++ code/app.js
@@ -2,6 +2,20 @@
 // let a = 2;
 // let b = 4;
 
-let a = (b = 4);
+// let a = (b = c = 15);
 
-console.log(a + b);
+// console.log(a + b + c);
+
+// let a = 2;
+// let b = 4;
+// let c = 10;
+
+// console.log(a);
+// console.log(b);
+// b = c;
+// console.log(a + b);
+
+let a = -1.0e1000;
+
+console.log(a / 'asd');
+console.log(typeof a);
